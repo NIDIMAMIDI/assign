cf5edbb2a2d171c91c3cb522c78c72d252b946ec
Sharuk <sharuks909@gmail.com>
2023-04-19 10:09:46 +0530

    first commit

---
+++ program.py
@@ -0,0 +1,24 @@
+
+#1. write a program to print the grade of a student based on the marks obtained.
+
+
+#                    ----->Program<-----
+marks = int(input("Enter the marks that you are obtained : "))
+if marks > 90 and marks <= 100:
+      print("A+")
+elif marks > 80 and marks <= 90 :
+      print("A")
+elif marks > 70 and marks <= 80 :
+      print("B+")
+elif marks > 60 and marks <= 70 :
+      print("B")
+elif marks > 50 and marks <= 60 :
+      print("C")
+elif marks > 40 and marks <= 50 :
+      print("A")
+else :
+      print("FAIL")
+
+
+
+
